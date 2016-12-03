5d502aff7ae167960605760046b4e9ab03ae22a1
Conny Yang <connyyang@connys-macbook-pro.local>
2016-12-03 15:09:40 +1100

    Initial Commit

---
+++ QMee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QMee
+//
+//  Created by Conny Yang on 3/12/2016.
+//  Copyright © 2016 Conny Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
